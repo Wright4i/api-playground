e4ce354f8a628389e14c0745bdc0210215c4f008
Joseph Wright <Joseph@Wright400.com>
2024-11-09 05:42:16 -0500

    Bug fetch_one not available on Session

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,8 @@ async def validate_token(tokens_db, authorization: str = Header(None)):
     if authorization and isinstance(authorization, str) and authorization.startswith("Bearer "):
         token = authorization.split(" ")[1]
         query = Token.__table__.select().where(Token.token == token)
-        user = await tokens_db.fetch_one(query)
+        user = tokens_db.execute(query)
+        user = user.fetchone()
         if user:
             return {"status": "VALID", "username": user["name"]}
     return {"status": "INVALID", "username": None}
